feb2a817dbf4d56694df9fe2e8e00a6194b8e220
Eloteck <eloteck@outlook.fr>
2015-06-07 21:08:38 +0200

    add info in article

--- app/views/article.php
+++ app/views/article.php
@@ -3,7 +3,13 @@
 	<img src='<?= $data[0]['img'] ?>' alt="Miniature"/>
 	<div class="article-content">
 		<h2><?= $data[0]['title'] ?></h2>
-		<p><?= $data[0]['creation_date'] ?></p>
+		<div class="article-info">
+			<p class="article-date"><?= $data[0]['creation_date'] ?></p>
+			<div class="void"></div>
+			<p class="article-tags"><?= $data[0]['tags'] ?></p>
+			<div class="void"></div>
+			<p class="article-category"><?= $data[0]['category'] ?></p>
+		</div>
 		<p><?= $data[0]['content'] ?></p>
 
 		<div id="disqus_thread"></div>
