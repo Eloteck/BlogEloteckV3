7d7a8877618dbe6791ae1d888964075248022d0b
Eloteck <eloteck@outlook.fr>
2015-06-07 19:41:22 +0200

    write article

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -18,7 +18,21 @@ class Admin extends Controller
 
 	public function write()
 	{
-		$this->view('admin/write');
+		if (isset($_POST['article_title']) && isset($_POST['article_url']) && isset($_POST['article_img']) && isset($_POST['article_content']) && isset($_POST['article_tags']) && isset($_POST['article_category']))
+		{
+			$send = $this->model('mod_Article');
+			$verif = $send->sendArticle($_POST['article_title'], $_POST['article_url'], $_POST['article_img'], $_POST['article_content'], $_POST['article_tags'], $_POST['article_category']);
+
+			if ($verif == true) {
+				$this->view('admin/write', array('success' => 'Votre article a bien été publié ! <a href="article/'.$_POST['article_url'].'">Lien vers l\'article</a>'));
+			}
+			else{
+				$this->view('admin/write', array('error' => 'Oops, tout ne s\'est pas bien passer. Veuillez rééssayer.'));
+			}
+		}
+		else{
+			$this->view('admin/write');
+		}
 	}
 
 }
--- app/views/admin/write.php
+++ app/views/admin/write.php
@@ -2,7 +2,20 @@
 <section class='admin_index box'>
 	<div class='box-content'>
 		<h1>Ecrire un article</h1>
-		<textarea id="editor">
-		</textarea>
+		<?php if (isset($data['success'])) : ?>
+			<div class="success-message">
+				<?= $data['success'] ?>
+			</div>
+		<?php endif ?>
+		<form method="POST" action="admin/write">
+			<input type="text" name="article_title" placeholder="Titre de l'article" required/>
+			<input type="text" name="article_url" placeholder="URL de l'article" required/>
+			<input type="text" name="article_img" placeholder="Image de l'article" required/>
+			<textarea id="editor" name="article_content"></textarea>
+			<input type="text" name="article_tags" placeholder="Tags" required/>
+			<input type="text" name="article_category" placeholder="Category" required/>
+			
+			<input type="submit" value="Envoyer" class="green button" />
+		</form>
 	</div>
 </section>
