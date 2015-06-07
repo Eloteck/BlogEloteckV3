3a5b6fb0cccb32703da29b7147001302a098e691
Eloteck <eloteck@outlook.fr>
2015-06-07 20:11:48 +0200

    Add tags and category in article presentation

--- app/views/home.php
+++ app/views/home.php
@@ -7,6 +7,10 @@
 			<h2><?= $new['title'] ?></h2>
 			<div class="article-info">
 				<p class="article-date"><?= $new['creation_date'] ?></p>
+				<div class="void"></div>
+				<p class="article-tags"><?= $new['tags'] ?></p>
+				<div class="void"></div>
+				<p class="article-category"><?= $new['category'] ?></p>
 			</div>
 			<p class="article-text"><?= $new['content'] ?>[...]</p>
 			<a href="article/<?= $new['url'] ?>" class="article-link">Lire l'article</a>
