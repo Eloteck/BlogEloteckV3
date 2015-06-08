eb2cfe6e56e6809c69f9ffba8d1803bcc8918df3
Eloteck <eloteck@outlook.fr>
2015-06-08 02:19:54 +0200

    Add location info in column

--- app/config/column.php
+++ app/config/column.php
@@ -1,8 +1,9 @@
 <?php
 
-return array(
+return [
 	
 	'img'		=> "../public/img/pp.png",
-	'desc'		=> "Jeune développeur web. 17 ans, bac STi2D SIN. J'aime les chatons, le chocolat, le hardware et l'informatique en général."
+	'desc'		=> "Jeune développeur web. 17 ans, bac STi2D SIN. J'aime les chatons, le chocolat, le hardware et l'informatique en général.",
 
-	);
+	'location'	=> "Toulouse, France"
+	];
